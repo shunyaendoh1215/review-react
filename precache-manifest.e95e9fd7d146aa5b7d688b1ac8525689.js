92e07df10b8747709ef26eec62d8ef0b7f6627e7
shunyaendoh1215 <shunyaendoh.1215@gmail.com>
2020-10-09 23:45:25 +0900

    Updates

---
+++ precache-manifest.e95e9fd7d146aa5b7d688b1ac8525689.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fa8b3883730388f49b3a7bcafe90216",
+    "url": "/review-react/index.html"
+  },
+  {
+    "revision": "04ecddd6fd332bd946ef",
+    "url": "/review-react/static/css/main.530513f7.chunk.css"
+  },
+  {
+    "revision": "6b3e2f70525e5d6ee2a2",
+    "url": "/review-react/static/js/2.3b507005.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/review-react/static/js/2.3b507005.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04ecddd6fd332bd946ef",
+    "url": "/review-react/static/js/main.094b26ef.chunk.js"
+  },
+  {
+    "revision": "41087f0a09f8a1c8862d",
+    "url": "/review-react/static/js/runtime-main.c2737874.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/review-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
